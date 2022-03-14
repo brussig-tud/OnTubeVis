17c617d470349a946f84df81bb1a31fa1ce815fe
Benjamin Russig <benjamin.russig@tu-dresden.de>
2022-03-14 22:45:06 +0100

    Make obd_handler actually return trajectory data

--- obd_handler.cxx
+++ obd_handler.cxx
@@ -14,19 +14,34 @@
 #include <cgv/utils/scan.h>
 #include <cgv/utils/advanced_scan.h>
 
-// local includes
-#include "regulargrid.h"
+// 3rd party libs
 #include <json.hpp>
+#include <peridetic.h>
 
 // implemented header
 #include "obd_handler.h"
 
 
+/// identifyier to use for position data
+#define OBD_POSITION_ATTRIB_NAME "position"
+
+/// identifyier to use for radius data
+#define OBD_ALTITUDE_ATTRIB_NAME "altitude"
+
+/// identifyier to use for radius data
+#define OBD_SPEED_ATTRIB_NAME "speed"
+
+/// identifyier to use for radius data
+#define OBD_RADIUS_ATTRIB_NAME "radius"
+
+/// identifyier to use for timestamp attribute
+#define OBD_TIME_ATTRIB_NAME "time"
+
 
 template <class flt_type>
 bool obd_handler<flt_type>::can_handle (std::istream &contents) const
 {
-	const stream_pos_guard g(contents); // do we need this???
+	const stream_pos_guard g(contents); // do we need this??? [Benjamin] I guess not (at least not anymore), but would have to verify a few things to make sure it's safe to remove
 	nlohmann::json j;
 	try
 	{
@@ -155,7 +170,66 @@ traj_dataset<flt_type> obd_handler<flt_type>::read (
 	for (auto tc : type_counts)
 		std::cout << "  " << tc.first << ": " << tc.second << std::endl;
 
-	return traj_dataset<flt_type>();
+
+	////
+	// transform loaded data into traj_mgr dataset
+
+	// perpare dataset container object
+	traj_dataset<flt_type> ret;
+	if (gps_infos.size() < 2)
+		return ret;
+	const visual_attribute_mapping<real> vamap({
+		{VisualAttrib::POSITION, {OBD_POSITION_ATTRIB_NAME}}, {VisualAttrib::RADIUS, {OBD_RADIUS_ATTRIB_NAME}}
+	});
+
+	// for float compatibility, we make everything relative to the first sample in terms of position and time
+	const auto refpos = peri::xyzForLpa(peri::LPA{gps_infos[0].latitude, gps_infos[0].longitude, gps_infos[0].altitude});
+	const auto reftime = gps_infos[0].timestamp;
+
+	// commit positions
+	auto P = add_attribute<vec3>(ret, OBD_POSITION_ATTRIB_NAME);
+	auto A = add_attribute<flt_type>(ret, OBD_ALTITUDE_ATTRIB_NAME);
+	auto V = add_attribute<flt_type>(ret, OBD_SPEED_ATTRIB_NAME);
+	auto T = add_attribute<flt_type>(ret, OBD_TIME_ATTRIB_NAME); // for now, commiting timestamps as their own attribute is the only way to have them selectable in the glyph layers
+	auto &Ptraj = trajectories(ret, P.attrib);
+	double seg_dist_accum = 0;
+	for (unsigned i=0; i<(unsigned)gps_infos.size(); i++)
+	{
+		const auto &gps_info = gps_infos[i];
+		const auto xyzpos = peri::xyzForLpa(peri::LPA{gps_info.latitude, gps_info.longitude, gps_info.altitude}),
+		           relpos = peri::LPA{xyzpos[0]-refpos[0], xyzpos[1]-refpos[1], xyzpos[1]-refpos[1]};
+		vec3 pos((flt_type)relpos[0], (flt_type)relpos[1], (flt_type)relpos[2]);
+		if (i > 0)
+		{
+			const auto &prev = P.data.values.back();
+			// eliminate duplicates - ToDo: why are there so many?
+			if ((pos-prev).sqr_length() < std::numeric_limits<float>::epsilon()*2)
+				continue;
+			seg_dist_accum += (pos - P.data.values.back()).length();
+		}
+		// ToDo: what do the timestamps mean? Their either in nanoseconds or there is centuries in between samples...
+		flt_type time = (flt_type)((gps_info.timestamp-reftime)/1000000000);
+		P.data.append(pos, time);
+		A.data.append((flt_type)gps_info.altitude, time);
+		V.data.append((flt_type)gps_info.gps_speed, time);
+		T.data.append(time, time);
+	}
+	Ptraj.emplace_back(range{0, P.data.num()});
+	trajectories(ret, A.attrib) = Ptraj;	// all attributes are sampled in sync with position, so we
+	trajectories(ret, V.attrib) = Ptraj;	// can duplicate the trajectory info from the positions to
+	trajectories(ret, T.attrib) = Ptraj;	// altitude, speed and time as well
+
+	// invent radii
+	set_avg_segment_length(ret, flt_type(seg_dist_accum/(P.data.num()-1)));
+	auto R = add_attribute<flt_type>(ret, OBD_RADIUS_ATTRIB_NAME);
+	R.data.values = std::vector<flt_type>(P.data.num(), ret.avg_segment_length()*real(0.125));
+	R.data.timestamps = P.data.timestamps;
+	trajectories(ret, R.attrib) = Ptraj;
+	// - visual attribute mapping
+	ret.set_mapping(vamap);
+
+	// done!
+	return std::move(ret);
 }
 
 
