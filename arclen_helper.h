324d2a93cb8092d469cbc5cc3e786b969387f738
Benjamin Russig <benjamin.russig@tu-dresden.de>
2022-03-21 14:18:39 +0100

    Fix redundant arclen_helper documentation

--- arclen_helper.h
+++ arclen_helper.h
@@ -33,14 +33,8 @@ namespace arclen
 		std::vector<cgv::render::render_types::mat4> s_to_t;
 	};
 
-	/// compute arclength approximation for all trajectory segments of all loaded datasets. The result
-	/// is one 4x4 matrix per segment, which encodes a 4-segment cubic Bezier spline approximating
-	/// the segment arclength. The data is ordered in accordance with the index pairs in the render
-	/// data of the trajectory manager, so the corresponding attribute indices can be found by doubling
-	/// the segment index, and renderers can access the computed approximations using e.g. a storage
-	/// buffer and the current primitive ID.
-	template <class flt_type>
-	parametrization compute_parametrization (const traj_manager<flt_type> &mgr);
+	/// compute arclength approximation for all trajectory segments of all loaded datasets.
+	template <class flt_type> parametrization compute_parametrization (const traj_manager<flt_type> &mgr);
 
 	/// create a GPU-side storage buffer holding the t→s arclength parametrization
 	cgv::render::vertex_buffer upload_renderdata (
