brussig-tud/OnTubeVis

commit 5cc998b10f19f90974c31b23c68c2173e6a7f0a2

Make OptiX integration robust to unsupported systems

tubes.cxx
@@ -507,8 +507,10 @@ void tubes::on_set(void *member_ptr) {
 		// ### BEGIN: OptiX integration
 		// ###############################
 
-		optix_update_accelds();
-		optix_update_pipeline();
+		if (optix.enabled) {
+			optix_update_accelds();
+			optix_update_pipeline();
+		}
 
 		// ###############################
 		// ###  END:  OptiX integration
@@ -723,6 +725,23 @@ void tubes::on_set(void *member_ptr) {
 		update_member(&test_dir[1]);
 		update_member(&test_dir[2]);
 	}
+
+	// ###############################
+	// ### BEGIN: OptiX integration
+	// ###############################
+
+	if (member_ptr == &optix.enabled && optix.enabled)
+	{
+		const bool success = optix_ensure_init(*get_context());
+		if (success && traj_mgr.has_data()) {
+			optix_update_accelds();
+			optix_update_pipeline();
+		}
+	}
+
+	// ###############################
+	// ###  END:  OptiX integration
+	// ###############################
 
 	// default implementation for all members
 	// - remaining logic
@@ -1429,27 +1448,15 @@ bool tubes::init (cgv::render::context &ctx)
 	// ### BEGIN: OptiX integration
 	// ###############################
 
-	// initialize CUDA
-	CUDA_CHECK_SET(cudaFree(0), success);
-
-	// initialize OptiX
-	OPTIX_CHECK_SET(optixInit(), success);
-
-	// specify OptiX device context options
-	OptixDeviceContextOptions options = {};
-	options.logCallbackFunction = &optix_log_cb;
-	options.logCallbackLevel = 4;
-	std::cerr << std::endl; // <-- Make sure the initial CUDA/OptiX message stream is preceded by an empty line
-
-	// associate a CUDA context (and therefore a specific GPU) with this device context
-	CUcontext cuCtx = 0;  // zero means take the default context (i.e. first best compatible device)
-	OPTIX_CHECK_SET(optixDeviceContextCreate(cuCtx, &options, &optix.context), success);
-
-	// setup optix launch environment
-	success = success && optix.outbuf_albedo.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
-	success = success && optix.outbuf_depth.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
-	success = success && optix_update_accelds();
-	success = success && optix_update_pipeline();
+	if (optix.enabled)
+	{
+		const bool success_optix = optix_ensure_init(ctx);
+		success = success && success_optix;
+		if (traj_mgr.has_data() && success_optix) {
+			success = success && optix_update_accelds();
+			success = success && optix_update_pipeline();
+		}
+	}
 
 	// ###############################
 	// ###  END:  OptiX integration
@@ -1480,6 +1487,43 @@ void tubes::optix_destroy_pipeline (void)
 	OPTIX_SAFE_DESTROY_PROGGROUP(optix.prg_hit);
 	OPTIX_SAFE_DESTROY_PROGGROUP(optix.prg_miss);
 	OPTIX_SAFE_DESTROY_PROGGROUP(optix.prg_raygen);
+}
+
+bool tubes::optix_ensure_init (context &ctx)
+{
+	// report OK if nothing needs to be done
+	if (optix.initialized)
+		return true;
+
+	// initialize CUDA
+	CUDA_CHECK_FAIL(cudaFree(0));
+
+	// initialize OptiX
+	OPTIX_CHECK_FAIL(optixInit());
+
+	// specify OptiX device context options
+	OptixDeviceContextOptions options = {};
+	options.logCallbackFunction = &optix_log_cb;
+	options.logCallbackLevel = 4;
+	std::cerr << std::endl; // <-- Make sure the initial CUDA/OptiX message stream is preceded by an empty line
+
+	// associate a CUDA context (and therefore a specific GPU) with this device context
+	bool success = true;
+	OPTIX_CHECK_SET(
+		optixDeviceContextCreate(
+			0, // zero means take the default context (i.e. first best compatible device)
+			&options, &optix.context
+		),
+		success
+	);
+
+	// setup optix launch environment
+	success = success && optix.outbuf_albedo.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
+	success = success && optix.outbuf_depth.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
+
+	// done!
+	optix.initialized = success;
+	return success;
 }
 
 bool tubes::optix_update_accelds (void)
@@ -2084,7 +2128,7 @@ void tubes::draw (cgv::render::context &ctx)
 
 			switch(debug.render_mode) {
 			case DRM_NONE:
-				if (!optix.enabled)
+				if (!optix.enabled || !optix.initialized)
 					draw_trajectories(ctx);
 				break;
 			case DRM_NODES:
@@ -2106,7 +2150,7 @@ void tubes::draw (cgv::render::context &ctx)
 			if (show_bbox)
 				bbox_rd.render(ctx, ref_box_renderer(ctx), bbox_style);
 
-			if (optix.enabled && debug.render_mode == DRM_NONE)
+			if (optix.enabled && optix.initialized && debug.render_mode==DRM_NONE)
 				optix_draw_trajectories(ctx);
 		}
 

tubes.h
@@ -71,6 +71,9 @@ protected:
 	// state
 	struct
 	{
+		// keep track of initialization state
+		bool initialized = false;
+
 		// use OptiX instead of OpenGL rasterization
 		bool enabled = false;
 
@@ -107,11 +110,12 @@ protected:
 		texture tex_albedo, tex_depth;
 	} optix;
 
+	void optix_destroy_accelds(void);
+	void optix_destroy_pipeline(void);
+
+	bool optix_ensure_init (context &ctx);
 	bool optix_update_accelds (void);
 	bool optix_update_pipeline (void);
-
-	void optix_destroy_accelds (void);
-	void optix_destroy_pipeline (void);
 
 	void optix_draw_trajectories (context &ctx);
 

commit 75aec07ba666db867e2696880ba8b3affd0da26c

Fix lazy initialization logic bug introduced in 5cc998b10f19f90974c31b23c68c2173e6a7f0a2

optix_integration.cxx
@@ -156,6 +156,7 @@ cuda_output_buffer<pxl_fmt>::cuda_output_buffer(CUDAOutputBufferType type, int w
 template <class pxl_fmt>
 cuda_output_buffer<pxl_fmt>::~cuda_output_buffer()
 {
+	m_height = m_width = 0;
 	if (initialized) try
 	{
 		initialized = false;

optix_integration.h
@@ -484,11 +484,11 @@ private:
 	bool initialized;
 
 	CUDAOutputBufferType m_type;
-	int m_width = 0u;
-	int m_height = 0u;
+	int m_width = 0;
+	int m_height = 0;
 
 	cudaGraphicsResource* m_cuda_gfx_resource = nullptr;
-	GLuint m_pbo = 0u;
+	GLuint m_pbo = 0;
 	pxl_fmt* m_device_pixels = nullptr;
     pxl_fmt* m_host_zcopy_pixels = nullptr;
 	std::vector<pxl_fmt>  m_host_pixels;

tubes.cxx
@@ -731,13 +731,7 @@ void tubes::on_set(void *member_ptr) {
 	// ###############################
 
 	if (member_ptr == &optix.enabled && optix.enabled)
-	{
-		const bool success = optix_ensure_init(*get_context());
-		if (success && traj_mgr.has_data()) {
-			optix_update_accelds();
-			optix_update_pipeline();
-		}
-	}
+		optix_ensure_init(*get_context());
 
 	// ###############################
 	// ###  END:  OptiX integration
@@ -1449,14 +1443,7 @@ bool tubes::init (cgv::render::context &ctx)
 	// ###############################
 
 	if (optix.enabled)
-	{
-		const bool success_optix = optix_ensure_init(ctx);
-		success = success && success_optix;
-		if (traj_mgr.has_data() && success_optix) {
-			success = success && optix_update_accelds();
-			success = success && optix_update_pipeline();
-		}
-	}
+		success = success && optix_ensure_init(ctx);
 
 	// ###############################
 	// ###  END:  OptiX integration
@@ -1516,8 +1503,14 @@ bool tubes::optix_ensure_init (context &ctx)
 		),
 		success
 	);
+	if (!success)
+		return false;
 
 	// setup optix launch environment
+	if (traj_mgr.has_data()) {
+		success = success && optix_update_accelds();
+		success = success && optix_update_pipeline();
+	}
 	success = success && optix.outbuf_albedo.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
 	success = success && optix.outbuf_depth.reset(CUOutBuf::GL_INTEROP, ctx.get_width(), ctx.get_height());
 
@@ -2009,10 +2002,13 @@ void tubes::init_frame (cgv::render::context &ctx)
 	// ### BEGIN: OptiX integration
 	// ###############################
 
-	// keep the optix interop buffer up to date with the viewport size
-	const unsigned w=ctx.get_width(), h=ctx.get_height();
-	optix.outbuf_albedo.resize(w, h);
-	optix.outbuf_depth.resize(w, h);
+	if (optix.initialized && optix.enabled)
+	{
+		// keep the optix interop buffer up to date with the viewport size
+		const unsigned w=ctx.get_width(), h=ctx.get_height();
+		optix.outbuf_albedo.resize(w, h);
+		optix.outbuf_depth.resize(w, h);
+	}
 
 	// ###############################
 	// ###  END:  OptiX integration

tubes.h
@@ -74,7 +74,7 @@ protected:
 		// keep track of initialization state
 		bool initialized = false;
 
-		// use OptiX instead of OpenGL rasterization
+		// use OptiX to raycast tubes instead of OpenGL rasterization
 		bool enabled = false;
 
 		// subdivide into quadratic beziers for higher ray tracing performance