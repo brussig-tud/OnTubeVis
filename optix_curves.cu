brussig-tud/OnTubeVis

commit 7cca7152658eafd1b7956a29e8b4be334fdb12cf

Optix code cleanup, prepare for changing intersection routine interface to world-space (from eye-space)

optix_curves.cu
@@ -220,8 +220,8 @@ extern "C" __global__ void __intersection__russig (void)
 
 	// perform intersection
 	const Hit hit = EvalSplineISect(
-		/*optixGetWorldRayOrigin(), */mul_mat_vec(params.cam_N, optixGetWorldRayDirection()),  // ray directions in eye-space
-		make_float3(nodes_eye[0]), make_float3(nodes_eye[1]), make_float3(nodes_eye[2]), radii[0], radii[1], radii[2]
+		optixGetWorldRayOrigin(), /*mul3_mat_vec(params.cam_N, */optixGetWorldRayDirection()/*)*/,  // ray directions in eye-space
+		/*make_float3(nodes_eye[0]), make_float3(nodes_eye[1]), make_float3(nodes_eye[2]), */nodes[0], nodes[1], nodes[2], radii[0], radii[1], radii[2]
 	);
 	if (hit.l < pos_inf)
 		// report our intersection
@@ -287,7 +287,7 @@ extern "C" __global__ void __closesthit__ch (void)
 
 	// compute hit normal in eye-space
 	const float3 pos_curve = curve.eval(ts);
-	const float3 normal = normalize(mul_mat_vec(params.cam_N,
+	const float3 normal = normalize(mul3_mat_vec(params.cam_N,
 #ifdef OTV_PRIMITIVE_RUSSIG
 		pos - pos_curve
 #else
@@ -306,13 +306,13 @@ extern "C" __global__ void __closesthit__ch (void)
 	{
 		cubic_interpolator_vec3 curve_orig;
 		curve_orig.from_hermite(n0.pos_rad, n0.tangent, n1.pos_rad, n1.tangent);
-		tangent = normalize(mul_mat_vec(params.cam_N, curve_orig.derive().eval(t)));
+		tangent = normalize(mul3_mat_vec(params.cam_N, curve_orig.derive().eval(t)));
 	}
 	else
 #else
 	const float3
 #endif
-	tangent = normalize(mul_mat_vec(params.cam_N, dcurve.eval(ts)));
+	tangent = normalize(mul3_mat_vec(params.cam_N, dcurve.eval(ts)));
 
 	// calculate pre-shading surface color
 	const float4 color = mix(n0.color, n1.color, t);

optix_isect.h
@@ -90,8 +90,8 @@ float EvalPolyD3_5(const float x, const float *c) { return EvalPoly(x, c[3] * 6.
 
 vec3 EvalCSpline(const vec3 &p1, const vec3 &t1, const vec3 &p2, const vec3 &t2, const float t)
 {
-	vec3 h1 = p1 + _1o3*t1;	// DANGEROUS EDIT!!!
-	vec3 h2 = p2 - _1o3*t2;	// DANGEROUS EDIT!!!
+	vec3 h1 = p1 + _1o3*t1;
+	vec3 h2 = p2 - _1o3*t2;
 
 	vec3 a1 = mix(p1, h1, t);
 	vec3 a2 = mix(h1, h2, t);
@@ -103,7 +103,6 @@ vec3 EvalCSpline(const vec3 &p1, const vec3 &t1, const vec3 &p2, const vec3 &t2,
 	return mix(b1, b2, t);
 }
 
-// DANGEROUS EDIT!!! -----------------------------------------------|---------|
 #define DEF_FINDROOTS_D1(N)                                                                         \
 void FindRootsD1(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_o)                  \
 {	                                                                                                \
@@ -113,13 +112,13 @@ void FindRootsD1(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 	unsigned j = 0;                                                                                 \
 	                                                                                                \
 	float x_l = x_i[0];                                                                             \
-	float y_l = EvalPolyD1_5(x_l, poly_C); /* DANGEROUS EDIT!!! */                                  \
-	float sy_l = sign(y_l);  /* DANGEROUS EDIT!!! */                                                \
+	float y_l = EvalPolyD1_5(x_l, poly_C);                                                          \
+	float sy_l = sign(y_l);                                                                         \
                                                                                                     \
 	for(unsigned i = 1; i < N; ++i) {                                                               \
 		float x_r = x_i[i];                                                                         \
-		float y_r = EvalPolyD1_5(x_r, poly_C); /* DANGEROUS EDIT!!! */                              \
-		float sy_r = sign(y_r); /* DANGEROUS EDIT!!! */                                             \
+		float y_r = EvalPolyD1_5(x_r, poly_C);                                                      \
+		float sy_r = sign(y_r);                                                                     \
 		                                                                                            \
 		x_o[i] = 0.f;                                                                               \
 		                                                                                            \
@@ -127,8 +126,8 @@ void FindRootsD1(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 			if(sy_l != sy_r) {                                                                      \
 				float n = x_l;                                                                      \
 				float p = x_r;                                                                      \
-				float ny = EvalPolyD1_5(n, poly_C); /* DANGEROUS EDIT!!! */                         \
-				float py = EvalPolyD1_5(p, poly_C); /* DANGEROUS EDIT!!! */                         \
+				float ny = EvalPolyD1_5(n, poly_C);                                                 \
+				float py = EvalPolyD1_5(p, poly_C);                                                 \
 				                                                                                    \
 				if(ny > 0.f && py < 0.f) {                                                          \
 					float t = n;                                                                    \
@@ -137,7 +136,7 @@ void FindRootsD1(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 				                                                                                    \
 				for(unsigned j = 0; j < ITERATION_COUNT; ++j) {                                     \
 					float m = (n + p) * 0.5f;                                                       \
-					float f = EvalPolyD1_5(m, poly_C); /* DANGEROUS EDIT!!! */                      \
+					float f = EvalPolyD1_5(m, poly_C);                                              \
 					                                                                                \
 					if(f < 0.f) n = m;                                                              \
 					else p = m;                                                                     \
@@ -161,7 +160,6 @@ void FindRootsD1(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 	x_o[N] = x_i[N - 1];                                                                            \
 }
 
-// DANGEROUS EDIT!!! -----------------------------------------------|---------|
 #define DEF_FINDROOTS_D0(N)                                                                         \
 void FindRootsD0(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_o)                  \
 {	                                                                                                \
@@ -171,13 +169,13 @@ void FindRootsD0(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 	unsigned j = 0;                                                                                 \
 	                                                                                                \
 	float x_l = x_i[0];                                                                             \
-	float y_l = EvalPolyD0_5(x_l, poly_C); /* DANGEROUS EDIT!!! */                                  \
-	float sy_l = sign(y_l);  /* DANGEROUS EDIT!!! */                                                \
+	float y_l = EvalPolyD0_5(x_l, poly_C);                                                          \
+	float sy_l = sign(y_l);                                                                         \
                                                                                                     \
 	for(unsigned i = 1; i < N; ++i) {                                                               \
 		float x_r = x_i[i];                                                                         \
-		float y_r = EvalPolyD0_5(x_r, poly_C); /* DANGEROUS EDIT!!! */                              \
-		float sy_r = sign(y_r); /* DANGEROUS EDIT!!! */                                             \
+		float y_r = EvalPolyD0_5(x_r, poly_C);                                                      \
+		float sy_r = sign(y_r);                                                                     \
 		                                                                                            \
 		x_o[i] = 0.f;                                                                               \
 		                                                                                            \
@@ -185,8 +183,8 @@ void FindRootsD0(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 			if(sy_l != sy_r) {                                                                      \
 				float n = x_l;                                                                      \
 				float p = x_r;                                                                      \
-				float ny = EvalPolyD0_5(n, poly_C); /* DANGEROUS EDIT!!! */                         \
-				float py = EvalPolyD0_5(p, poly_C); /* DANGEROUS EDIT!!! */                         \
+				float ny = EvalPolyD0_5(n, poly_C);                                                 \
+				float py = EvalPolyD0_5(p, poly_C);                                                 \
 				                                                                                    \
 				if(ny > 0.f && py < 0.f) {                                                          \
 					float t = n;                                                                    \
@@ -195,7 +193,7 @@ void FindRootsD0(float poly_C[N1], float x_i[N], int m_i[N], float *x_o, int *m_
 				                                                                                    \
 				for(unsigned j = 0; j < ITERATION_COUNT; ++j) {                                     \
 					float m = (n + p) * 0.5f;                                                       \
-					float f = EvalPolyD0_5(m, poly_C); /* DANGEROUS EDIT!!! */                      \
+					float f = EvalPolyD0_5(m, poly_C);                                              \
 					                                                                                \
 					if(f < 0.f) n = m;                                                              \
 					else p = m;                                                                     \
@@ -227,7 +225,7 @@ vec3 GetOrthoVec(const vec3 &v)
     return abs(v.x) > abs(v.z) ? make_float3(-v.y, v.x, 0.f) : make_float3(0.f, -v.z, v.y);
 }
 
-void SplinePointsToPolyCoeffs(float p0, float h, float p1, float *o_c) // <-- DANGEROUS EDIT!!!
+void SplinePointsToPolyCoeffs(float p0, float h, float p1, float *o_c)
 {
 	o_c[0] = p0;
 	o_c[1] = -2.f * p0 + 2.f * h;
@@ -238,15 +236,15 @@ vec3 qSplineEval(float l, float curveX[N0], float curveY[N0], float curveZ[N0])
 {
 	return make_float3(
 		EvalPolyD0_3(l, curveX),
-		EvalPolyD0_3(l, curveY),	// DANGEROUS EDITS!!!
+		EvalPolyD0_3(l, curveY),
 		EvalPolyD0_3(l, curveZ)
 	);
 }
 
 float qSplineIDistEval(float t, float curveX[N0], float polyB_C[N1])
 {		
-	float term  = EvalPolyD0_3(t, curveX);  // DANGEROUS EDITS!!!
-	float discr = EvalPolyD0_5(t, polyB_C); // DANGEROUS EDITS!!!
+	float term  = EvalPolyD0_3(t, curveX);
+	float discr = EvalPolyD0_5(t, polyB_C);
 		
 	if(discr < 0.f) return pos_inf;
 	else return term - sqrt(discr);
@@ -345,7 +343,7 @@ __device__ __forceinline__ float3 mul_vec_mat3 (const float3 &vec, const float *
 	return r;
 }
 
-Hit EvalSplineISect(/*const vec3 &ray_orig, */const vec3 &dir, vec3 s, vec3 h, vec3 t, const float rs, const float rh, const float rt)
+Hit EvalSplineISect(const vec3 &ray_orig, const vec3 &dir, vec3 s, vec3 h, vec3 t, const float rs, const float rh, const float rt)
 {
 	Hit hit;
 	hit.t = 0.f;
@@ -357,10 +355,17 @@ Hit EvalSplineISect(/*const vec3 &ray_orig, */const vec3 &dir, vec3 s, vec3 h, v
 	set_mat3_col(RM3, 0, dir);
 	set_mat3_col(RM3, 1, normalize(GetOrthoVec(dir)));
 	set_mat3_col(RM3, 2, cross(dir, get_mat3_col(RM3, 1)));
+	/*float RS[16];
+	{ const float3 dir_ortho = normalize(GetOrthoVec(dir));
+	  make_local_frame(RS, dir, dir_ortho, cross(dir, dir_ortho), ray_orig); }*/
+
 	// - transform control points
 	s = mul_vec_mat3(s, RM3);
 	t = mul_vec_mat3(t, RM3);
 	h = mul_vec_mat3(h, RM3);
+	/*s = mul3_pos_mat(s, RS);
+	t = mul3_pos_mat(t, RS);
+	h = mul3_pos_mat(h, RS);*/
 
 	float curveX[N0];
 	float curveY[N0];

optix_tools.h
@@ -89,7 +89,7 @@ __device__ __forceinline__ unsigned pack_unorm_4x8 (const float4 v)
 }
 
 // set a column of the given 4x4 matrix to the given vector
-__device__ __forceinline__ void set_mat_colp (float *mat, const unsigned col, const float4 &vec)
+__device__ __forceinline__ void set_mat_col (float *mat, const unsigned col, const float4 &vec)
 {
 	*(((float4*)mat) + col) = vec;
 }
@@ -125,7 +125,7 @@ __device__ __forceinline__ const float3& get_mat_col3 (const float* mat, const u
 }
 
 // right-multiply R^3 position vector to 4x4 homogenous transformation matrix, return
-// result as homogenous R^3 position vector with w-component
+// result as homogenous R^3 vector with w-component
 __device__ __forceinline__ float4 mul_mat_pos (const float *mat, const float3 &pos)
 {
 	float4 r;
@@ -135,8 +135,19 @@ __device__ __forceinline__ float4 mul_mat_pos (const float *mat, const float3 &p
 	r.w = mat[3]*pos.x + mat[7]*pos.y + mat[11]*pos.z + mat[15];
 	return r;
 }
+// right-multiply R^3 position vector to 4x4 homogenous transformation matrix, return
+// result as 3D position vector with implied homogenous coordinate w=1
+__device__ __forceinline__ float3 mul3_mat_pos(const float* mat, const float3& pos)
+{
+	float3 r;
+	r.x = mat[0]*pos.x + mat[4]*pos.y + mat[ 8]*pos.z + mat[12];
+	r.y = mat[1]*pos.x + mat[5]*pos.y + mat[ 9]*pos.z + mat[13];
+	r.z = mat[2]*pos.x + mat[6]*pos.y + mat[10]*pos.z + mat[14];
+	return r;
+}
+
 // left-multiply R^3 position vector to 4x4 homogenous transformation matrix, return
-// result as homogenous R^3 position vector with w-component
+// result as homogenous R^3 vector with w-component
 __device__ __forceinline__ float4 mul_pos_mat (const float3 &pos, const float *mat)
 {
 	float4 r;
@@ -146,9 +157,31 @@ __device__ __forceinline__ float4 mul_pos_mat (const float3 &pos, const float *m
 	r.w = mat[12]*pos.x + mat[13]*pos.y + mat[14]*pos.z + mat[15];
 	return r;
 }
+// left-multiply R^3 position vector to 4x4 homogenous transformation matrix, return
+//result as 3D position vector with implied homogenous coordinate w=1
+__device__ __forceinline__ float3 mul3_pos_mat (const float3 &pos, const float *mat)
+{
+	float3 r;
+	r.x = mat[0]*pos.x + mat[1]*pos.y + mat[ 2]*pos.z + mat[ 3];
+	r.y = mat[4]*pos.x + mat[5]*pos.y + mat[ 6]*pos.z + mat[ 7];
+	r.z = mat[8]*pos.x + mat[9]*pos.y + mat[10]*pos.z + mat[11];
+	return r;
+}
 
-// right-multiply R^3 (direction-)vector to 4x4 homogenous transformation matrix
-__device__ __forceinline__ float3 mul_mat_vec (const float *mat, const float3 &vec)
+// right-multiply R^3 (direction-)vector to 4x4 homogenous transformation matrix, return
+// result as homogenous R^3 vector with w-component
+__device__ __forceinline__ float4 mul_mat_vec (const float *mat, const float3 &vec)
+{
+	float4 r;
+	r.x = mat[0]*vec.x + mat[4]*vec.y + mat[ 8]*vec.z;
+	r.y = mat[1]*vec.x + mat[5]*vec.y + mat[ 9]*vec.z;
+	r.z = mat[2]*vec.x + mat[6]*vec.y + mat[10]*vec.z;
+	r.w = mat[3]*vec.x + mat[7]*vec.y + mat[11]*vec.z;
+	return r;
+}
+// right-multiply R^3 (direction-)vector to 4x4 homogenous transformation matrix, return
+// result as 3D direction vector with implied homogenous coordinate w=0
+__device__ __forceinline__ float3 mul3_mat_vec (const float *mat, const float3 &vec)
 {
 	float3 r;
 	r.x = mat[0]*vec.x + mat[4]*vec.y + mat[ 8]*vec.z;
@@ -156,8 +189,21 @@ __device__ __forceinline__ float3 mul_mat_vec (const float *mat, const float3 &v
 	r.z = mat[2]*vec.x + mat[6]*vec.y + mat[10]*vec.z;
 	return r;
 }
+
+// left-multiply R^3 (direction-)vector to 4x4 homogenous transformation matrix, return
+// result as homogenous R^3 vector with w-component
+__device__ __forceinline__ float4 mul_vec_mat (const float3 &vec, const float *mat)
+{
+	float4 r;
+	r.x = mat[ 0]*vec.x + mat[ 1]*vec.y + mat[ 2]*vec.z;
+	r.y = mat[ 4]*vec.x + mat[ 5]*vec.y + mat[ 6]*vec.z;
+	r.z = mat[ 8]*vec.x + mat[ 9]*vec.y + mat[10]*vec.z;
+	r.w = mat[12]*vec.x + mat[13]*vec.y + mat[14]*vec.z;
+	return r;
+}
 // left-multiply R^3 (direction-)vector to 4x4 homogenous transformation matrix
-__device__ __forceinline__ float3 mul_vec_mat (const float3 &vec, const float *mat)
+// result as 3D direction vector with implied homogenous coordinate w=0
+__device__ __forceinline__ float3 mul3_vec_mat (const float3 &vec, const float *mat)
 {
 	float3 r;
 	r.x = mat[0]*vec.x + mat[1]*vec.y + mat[ 2]*vec.z;
@@ -170,8 +216,8 @@ __device__ __forceinline__ float3 mul_vec_mat (const float3 &vec, const float *m
 __device__ __forceinline__ float4 mul_mat_vec (const float *mat, const float4 &vec)
 {
 	float4 r;
-	r.x = mat[0]*vec.x + mat[4]*vec.y + mat[8]*vec.z + mat[12]*vec.w;
-	r.y = mat[1]*vec.x + mat[5]*vec.y + mat[9]*vec.z + mat[13]*vec.w;
+	r.x = mat[0]*vec.x + mat[4]*vec.y + mat[ 8]*vec.z + mat[12]*vec.w;
+	r.y = mat[1]*vec.x + mat[5]*vec.y + mat[ 9]*vec.z + mat[13]*vec.w;
 	r.z = mat[2]*vec.x + mat[6]*vec.y + mat[10]*vec.z + mat[14]*vec.w;
 	r.w = mat[3]*vec.x + mat[7]*vec.y + mat[11]*vec.z + mat[15]*vec.w;
 	return r;
@@ -185,6 +231,15 @@ __device__ __forceinline__ float4 mul_vec_mat (const float4 &vec, const float *m
 	r.z = mat[ 8]*vec.x + mat[ 9]*vec.y + mat[10]*vec.z + mat[11]*vec.w;
 	r.w = mat[12]*vec.x + mat[13]*vec.y + mat[14]*vec.z + mat[15]*vec.w;
 	return r;
+}
+
+// create a system transformation matrix into the given local reference frame from basis vector and origin
+__device__ __forceinline__ void make_local_frame (float *mat, const float3 &x, const float3 &y, const float3 &z, const float3 &o)
+{
+	mat[0] = x.x; mat[4] = x.y; mat[ 8] = x.z;	mat[12] = -x.x*o.x - x.y*o.y - x.z*o.z;
+	mat[1] = y.y; mat[5] = y.y; mat[ 7] = y.y;	mat[13] = -y.x*o.x - y.y*o.y - y.z*o.z;
+	mat[2] = z.z; mat[6] = z.z; mat[10] = z.z;	mat[14] = -z.x*o.x - z.y*o.y - z.z*o.z;
+	mat[3] = 0; mat[7] = 0; mat[11] = 0;	mat[15] = 1;
 }
 
 // apply w-clip to homogenous R^3 vector, returning ordinary R^3 vector

commit d90da59e23f0dde8cf24fd3ce3346b4ae5e06984

Fix regression that broke custom intersector from 7cca7152658eafd1b7956a29e8b4be334fdb12cf

optix_curves.cu
@@ -220,8 +220,8 @@ extern "C" __global__ void __intersection__russig (void)
 
 	// perform intersection
 	const Hit hit = EvalSplineISect(
-		optixGetWorldRayOrigin(), /*mul3_mat_vec(params.cam_N, */optixGetWorldRayDirection()/*)*/,  // ray directions in eye-space
-		/*make_float3(nodes_eye[0]), make_float3(nodes_eye[1]), make_float3(nodes_eye[2]), */nodes[0], nodes[1], nodes[2], radii[0], radii[1], radii[2]
+		optixGetWorldRayOrigin(), mul3_mat_vec(params.cam_N, optixGetWorldRayDirection()),  // ray directions in eye-space
+		make_float3(nodes_eye[0]), make_float3(nodes_eye[1]), make_float3(nodes_eye[2])/*, nodes[0], nodes[1], nodes[2]*/, radii[0], radii[1], radii[2]
 	);
 	if (hit.l < pos_inf)
 		// report our intersection

optix_integration.cxx
@@ -35,7 +35,8 @@ std::vector<char> compile_cu2ptx (
 )
 {
 	// CUDA runtime compiler include directories
-	const std::string mydir = std::filesystem::path(filename).parent_path().string();
+	std::string mydir = std::filesystem::path(filename).parent_path().string();
+	if (mydir.empty()) mydir = ".";
 	const std::vector<std::string> include_args = {
 		"-I" CUDA_RUNTIME_COMPILER__INC_DIR_CUDA, "-I" CUDA_RUNTIME_COMPILER__INC_DIR_OPTIX,
 		"-I" CUDA_RUNTIME_COMPILER__INC_DIR_OPTIXSDK, "-I" CUDA_RUNTIME_COMPILER__INC_DIR_OPTIXSDK_CUDA,