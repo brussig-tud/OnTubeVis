brussig-tud/OnTubeVis

commit 8c2b2ff52c891300e5482855df5e98927b2266cc

Eliminate most compiler warnings caused by OnTubeVis code

demo.h
@@ -92,7 +92,7 @@ struct demo : public traj_format_handler<float>
 		return _pole.point;
 	}
 	template <>
-	static const float& pole<float> (void)
+	const float& pole<float> (void)
 	{
 		static const float _1 = 1;
 		return _1;
@@ -119,7 +119,7 @@ struct demo : public traj_format_handler<float>
 	template <class T>
 	inline static float len (const T &v) { return v.length(); }
 	template <>
-	inline static float len<float> (const float &v) { return std::abs(v); }
+	inline float len<float> (const float &v) { return std::abs(v); }
 
 	/// generic normalization template
 	template <class T>
@@ -161,7 +161,7 @@ struct demo : public traj_format_handler<float>
 		return std::move(result);
 	}
 	template <>
-	static float gen_unit_point<float> (std::mt19937&)
+	float gen_unit_point<float> (std::mt19937&)
 	{
 		// in the 1D case, we ignore direction and just return the unit ("north" on the 1-hypersphere). Negative "directions" can
 		// be achieved by providing appropriate mean and sigma values at the top level generator function .
@@ -214,7 +214,7 @@ struct demo : public traj_format_handler<float>
 		return result - pole<T>();
 	}
 	template <>
-	static float gen_dir_offset<float> (float, std::mt19937&)
+	float gen_dir_offset<float> (float, std::mt19937&)
 	{
 		// We ignore direction completely - just return a zero-offset
 		return 0;
@@ -230,7 +230,7 @@ struct demo : public traj_format_handler<float>
 	}
 	/// special handling of scalar case for efficiency (we don't call the direction-related stuff which wouldn't do anything anyway)
 	template <>
-	static float gen_vel_perturbation<float>(float t, const float&, float, const noise_function &noise, std::mt19937&)
+	float gen_vel_perturbation<float>(float t, const float&, float, const noise_function &noise, std::mt19937&)
 	{
 		return noise(t);
 	}

CMakeLists.txt
@@ -15,7 +15,7 @@ option(ENABLE_OPTIX_SUPPORT ${ENABLE_OPTIX_SUPPORT_DOC} ON)
 include(FetchContent)
 FetchContent_Declare(
 	cgv GIT_REPOSITORY https://github.com/sgumhold/cgv.git
-	GIT_TAG 7bbb1231f5c4c584db5de11690976dc5807dc7c7
+	GIT_TAG 521db9ffc4a61be70bb5ec2c2ea3535314840304
 )
 FetchContent_MakeAvailable(cgv)
 

glyph_shapes.h
@@ -58,6 +58,8 @@ class glyph_shape : public cgv::render::render_types {
 public:
 	typedef std::vector<glyph_attribute> attribute_list;
 
+	virtual ~glyph_shape() {}
+
 	virtual glyph_shape* clone() const = 0;
 
 	virtual GlyphType type() const = 0;
@@ -288,7 +290,7 @@ public:
 		
 		float as = sin(angle);
 		float ac = cos(angle);
-		mat2 R = (ac, as, -as, ac);
+		mat2 R = {ac, as, -as, ac};
 		
 		float half_base_width = 0.5f * param_values[0];
 		float half_height = 0.5f * param_values[1];

on_tube_vis.cxx
@@ -1239,7 +1239,7 @@ bool on_tube_vis::read_layer_configuration(const std::string& file_name) {
 					gam.set_attrib_source_index(static_cast<size_t>(shape_attrib_idx), attrib_idx);
 				}
 
-				if(attrib.modifiers && GAM_GLOBAL) {
+				if(attrib.modifiers & GAM_GLOBAL) {
 					it = tag.attributes.find("value");
 					if(it != end) {
 						std::string str = (*it).second;

commit 4bc3b3a753b3cdfa217ee9cbdfd33e3bd0a34e66

Fix MSVC compile error caused by 8c2b2ff52c891300e5482855df5e98927b2266cc

demo.h
@@ -13,6 +13,14 @@
 #include <traj_loader.h>
 
 
+// Workaround Clang/VS differing opinions of C++ standard
+#ifdef _MSC_VER
+	#define STATIC_TEMPLATE_SPECIALIZATION static
+#else
+	#define STATIC_TEMPLATE_SPECIALIZATION
+#endif
+
+
 /// represents logical trajectory data
 struct demo : public traj_format_handler<float>
 {
@@ -92,7 +100,7 @@ struct demo : public traj_format_handler<float>
 		return _pole.point;
 	}
 	template <>
-	const float& pole<float> (void)
+	STATIC_TEMPLATE_SPECIALIZATION const float& pole<float> (void)
 	{
 		static const float _1 = 1;
 		return _1;
@@ -161,7 +169,7 @@ struct demo : public traj_format_handler<float>
 		return std::move(result);
 	}
 	template <>
-	float gen_unit_point<float> (std::mt19937&)
+	STATIC_TEMPLATE_SPECIALIZATION float gen_unit_point<float> (std::mt19937&)
 	{
 		// in the 1D case, we ignore direction and just return the unit ("north" on the 1-hypersphere). Negative "directions" can
 		// be achieved by providing appropriate mean and sigma values at the top level generator function .
@@ -214,7 +222,7 @@ struct demo : public traj_format_handler<float>
 		return result - pole<T>();
 	}
 	template <>
-	float gen_dir_offset<float> (float, std::mt19937&)
+	STATIC_TEMPLATE_SPECIALIZATION float gen_dir_offset<float> (float, std::mt19937&)
 	{
 		// We ignore direction completely - just return a zero-offset
 		return 0;
@@ -230,7 +238,7 @@ struct demo : public traj_format_handler<float>
 	}
 	/// special handling of scalar case for efficiency (we don't call the direction-related stuff which wouldn't do anything anyway)
 	template <>
-	float gen_vel_perturbation<float>(float t, const float&, float, const noise_function &noise, std::mt19937&)
+	STATIC_TEMPLATE_SPECIALIZATION float gen_vel_perturbation<float>(float t, const float&, float, const noise_function &noise, std::mt19937&)
 	{
 		return noise(t);
 	}