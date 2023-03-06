brussig-tud/OnTubeVis

commit afb2d4d98cb87b6043669e5ed4da209dfa4dfbc8

reflect latest framework changes; fixed GPU voxelization

on_tube_vis.cxx
@@ -2463,12 +2463,11 @@ void on_tube_vis::create_density_volume(context& ctx, unsigned resolution) {
 	cgv::utils::stopwatch s(true);
 
 	if(voxelize_gpu) {
-		// prepare SSBO and index buffer handles
+		// prepare index buffer pointer
 		auto &tstr = cgv::render::ref_textured_spline_tube_renderer(ctx);
-		const int node_idx_handle = tstr.get_vbo_handle(ctx, render.aam, "node_ids");
-		const int data_handle = render.render_sbo.handle ? (const int&)render.render_sbo.handle - 1 : 0;
-		if(node_idx_handle > 0 && data_handle > 0)
-			density_volume.compute_density_volume_gpu(ctx, render.data, render.style.radius_scale, node_idx_handle, data_handle, density_tex);
+		const vertex_buffer* node_idx_buffer_ptr = tstr.get_vertex_buffer_ptr(ctx, render.aam, "node_ids");
+		if(node_idx_buffer_ptr && render.render_sbo.is_created())
+			density_volume.compute_density_volume_gpu(ctx, render.data, render.style.radius_scale, *node_idx_buffer_ptr, render.render_sbo, density_tex);
 	} else {
 		density_volume.compute_density_volume(render.data, render.style.radius_scale);
 
@@ -2558,7 +2557,7 @@ void on_tube_vis::draw_trajectories(context& ctx)
 	texture& tex_depth = *fbc.attachment_texture_ptr("depth");
 #endif
 	// - node attribute data needed by both rasterization and raytracing
-	int node_idx_handle = tstr.get_vbo_handle(ctx, render.aam, "node_ids");
+	const vertex_buffer* node_idx_buffer_ptr = tstr.get_vertex_buffer_ptr(ctx, render.aam, "node_ids");
 	// - TAA data that needs to be accessed across local scopes
 	mat4 curr_projection_matrix, curr_modelview_matrix;
 
@@ -2587,15 +2586,13 @@ void on_tube_vis::draw_trajectories(context& ctx)
 		// render tubes
 		auto &tstr = cgv::render::ref_textured_spline_tube_renderer(ctx);
 
-		// prepare SSBO handles
-		const int data_handle = render.render_sbo.handle ? (const int&)render.render_sbo.handle - 1 : 0,
-				  arclen_handle = render.arclen_sbo.handle ? (const int&)render.arclen_sbo.handle - 1 : 0;
-		int segment_idx_handle = tstr.get_index_buffer_handle(render.aam);
+		// prepare index buffer pointer
+		const vertex_buffer* segment_idx_buffer_ptr = tstr.get_index_buffer_ptr(render.aam);
 
-		if (!data_handle ||
-			!arclen_handle ||
-			segment_idx_handle < 0 ||
-			node_idx_handle < 0)
+		if(!render.render_sbo.is_created() ||
+			!render.arclen_sbo.is_created() ||
+			segment_idx_buffer_ptr == nullptr ||
+			node_idx_buffer_ptr == nullptr)
 			return;
 
 		// onyl perform a new visibility sort step when the view configuration deviates significantly
@@ -2612,7 +2609,7 @@ void on_tube_vis::draw_trajectories(context& ctx)
 		if(debug.sort && do_sort && !debug.force_initial_order) {
 			// measure sort time
 			//render.sorter.begin_time_query();
-			render.sorter.execute(ctx, data_handle, segment_idx_handle, eye_pos, view_dir, node_idx_handle);
+			render.sorter.execute(ctx, render.render_sbo, *segment_idx_buffer_ptr, eye_pos, view_dir, node_idx_buffer_ptr);
 			//benchmark.sort_time_total += render.sorter.end_time_query();
 			++benchmark.num_sorts;
 		}
@@ -2629,12 +2626,11 @@ void on_tube_vis::draw_trajectories(context& ctx)
 			count = static_cast<int>(debug.render_count);
 		}
 
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, data_handle);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, arclen_handle);
+		render.render_sbo.bind(ctx, VBT_STORAGE, 0);
+		render.arclen_sbo.bind(ctx, VBT_STORAGE, 1);
 		if (render.style.attrib_mode != textured_spline_tube_render_style::AM_ALL) {
-			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, node_idx_handle);
+			node_idx_buffer_ptr->bind(ctx, VBT_STORAGE, 2);
 			tstr.render(ctx, 0, count);
-			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, 0);
 		}
 		else
 			tstr.render(ctx, 0, count);

glsl/voxelize.glcs
@@ -20,6 +20,7 @@ struct data_type {
 	vec4 pos_rad;
 	vec4 color;
 	vec4 tangent;
+	vec4 t;
 };
 
 layout(std430, binding = 2) readonly buffer data_buffer {

voxelizer.cxx
@@ -149,7 +149,7 @@ void voxelizer::compute_density_volume(const traj_manager<float>::render_data *d
 	clamp_density();
 }
 
-void voxelizer::compute_density_volume_gpu(cgv::render::context& ctx, const traj_manager<float>::render_data *data_set, const float radius_scale, GLuint index_buffer, GLuint data_buffer, cgv::render::texture& tex) {
+void voxelizer::compute_density_volume_gpu(cgv::render::context& ctx, const traj_manager<float>::render_data *data_set, const float radius_scale, const cgv::render::vertex_buffer& index_buffer, const cgv::render::vertex_buffer& data_buffer, cgv::render::texture& tex) {
 
 	if(!data_set) {
 		std::cout << "Warning: compute_density_volume_gpu received nullptr for data_set." << std::endl;
@@ -205,8 +205,8 @@ void voxelizer::compute_density_volume_gpu(cgv::render::context& ctx, const traj
 	voxelize_prog.set_uniform(ctx, "vvol", vg.voxel_size*vg.voxel_size*vg.voxel_size);
 	voxelize_prog.set_uniform(ctx, "radius_scale", radius_scale);
 
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, index_buffer);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, data_buffer);
+	index_buffer.bind(ctx, cgv::render::VBT_STORAGE, 1);
+	data_buffer.bind(ctx, cgv::render::VBT_STORAGE, 2);
 
 	glDispatchCompute((GLuint)ceil(primitive_count / 64.0f), 1, 1);
 	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
@@ -225,8 +225,7 @@ void voxelizer::compute_density_volume_gpu(cgv::render::context& ctx, const traj
 	clamp_prog.disable(ctx);
 
 	glBindImageTexture(0, 0, 0, GL_TRUE, 0, GL_READ_WRITE, GL_R32F);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, 0);
-
+	
 	// calculate the mipmap via a compute shader
 	generate_mipmap(ctx, texture_handle);
 

voxelizer.h
@@ -5,6 +5,7 @@
 #include <cgv/render/render_types.h>
 #include <cgv/render/context.h>
 #include <cgv/render/shader_library.h>
+#include <cgv/render/vertex_buffer.h>
 #include <cgv_gl/gl/gl_context.h>
 
 #include "voxel_grid.h"
@@ -46,5 +47,5 @@ public:
 
 	void compute_density_volume(const traj_manager<float>::render_data *data_set, const float radius_scale);
 
-	void compute_density_volume_gpu(cgv::render::context& ctx, const traj_manager<float>::render_data *data_set, const float radius_scale, GLuint index_buffer, GLuint data_buffer, cgv::render::texture& tex);
+	void compute_density_volume_gpu(cgv::render::context& ctx, const traj_manager<float>::render_data *data_set, const float radius_scale, const cgv::render::vertex_buffer& index_buffer, const cgv::render::vertex_buffer& data_buffer, cgv::render::texture& tex);
 };

commit 591edb0cf96efd94e24089815bb9148b8a5cd26c

Fix compiler error when OptiX support is enabled caused by afb2d4d98cb87b6043669e5ed4da209dfa4dfbc8

on_tube_vis.cxx
@@ -1679,19 +1679,19 @@ bool on_tube_vis::optix_register_resources (context &ctx)
 
 	// register the SSBOs with CUDA
 	// - node data
-	const GLuint nodes_handle = (const int&)render.render_sbo.handle - 1;
+	const GLuint nodes_handle = (const int&)render.render_sbo.handle-1;
 	CUDA_CHECK_SET(
 		cudaGraphicsGLRegisterBuffer(&optix.sbo_nodes, nodes_handle, cudaGraphicsRegisterFlagsReadOnly),
 		success
 	);
 	// - node indices
-	const GLuint nodeids_handle = ref_textured_spline_tube_renderer(ctx).get_vbo_handle(ctx, render.aam, "node_ids");
+	const auto nodeids_vbo = ref_textured_spline_tube_renderer(ctx).get_vertex_buffer_ptr(ctx, render.aam, "node_ids");
 	CUDA_CHECK_SET(
-		cudaGraphicsGLRegisterBuffer(&optix.sbo_nodeids, nodeids_handle, cudaGraphicsRegisterFlagsReadOnly),
+		cudaGraphicsGLRegisterBuffer(&optix.sbo_nodeids, (const int&)(nodeids_vbo->handle)-1, cudaGraphicsRegisterFlagsReadOnly),
 		success
 	);
 	// - arclength
-	const GLuint alen_handle = (const int&)render.arclen_sbo.handle - 1;
+	const GLuint alen_handle = (const int&)render.arclen_sbo.handle-1;
 	CUDA_CHECK_SET(
 		cudaGraphicsGLRegisterBuffer(&optix.sbo_alen, alen_handle, cudaGraphicsRegisterFlagsReadOnly),
 		success