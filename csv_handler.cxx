brussig-tud/OnTubeVis

commit 906d5b2bda11c2135af1d95e6cb50c1538cd46dd

csv_handler: Hack in "virtual" attributes containing absolute values of vorticity vector components in case of Paraview streamline files

csv_handler.cxx
@@ -402,6 +402,34 @@ traj_dataset<flt_type> csv_handler<flt_type>::read (
 	if (timestamp_id > -1)
 		timestamp_format = Impl::guess_timestamp_format(fields[declared_attribs[timestamp_id].field_ids.front()]);
 
+	///////////
+	/// XXX: Hack to get absolute values of vorticity vector components for Paraview-exported Streamline datasets
+	///      Note: has more custom hacks inside main parser loop to take absolute value
+
+	static const csv_descriptor::attribute abs_vorticity[] = {
+		{"|Vorticity.x|", {"Vorticity:0", false, 5}},
+		{"|Vorticity.y|", {"Vorticity:1", false, 6}},
+		{"|Vorticity.z|", {"Vorticity:2", false, 7}}
+	};
+	bool is_paraview_streamline = false;
+	if (impl.csv_desc.name().compare("Paraview Streamline") == 0)
+	{
+		for (const auto &vattr : abs_vorticity)
+		{
+			declared_attribs.emplace_back(vattr);
+			auto &attrib = declared_attribs.back();
+			for (const auto &col : vattr.columns)
+			{
+				attrib.field_ids.emplace_back(col.number);
+				undeclared_cols.erase(col.number);
+			}
+		}
+		is_paraview_streamline = true;
+	}
+
+	/// \END hack
+	///////////
+
 	// parse the stream until EOF
 	bool nothing_loaded = true;
 	real dist_accum = 0;
@@ -467,6 +495,26 @@ traj_dataset<flt_type> csv_handler<flt_type>::read (
 				case 1:
 				{
 					auto &a = Impl::ensure_traj(attrib.trajs, traj_id, 1);
+
+					///////////
+					/// XXX: Hack to get absolute values of vorticity vector components for Paraview-exported Streamline datasets
+					///      Note: also has a preparatory custom hack in the initialization phase
+
+					if (is_paraview_streamline && [&attrib] {
+						for (const auto &vattr : abs_vorticity)
+							if (&attrib.desc == &vattr) // <-- this works because attrib just references the underlying csv_desc
+								return true;
+						return false;
+					}()) {
+						a.template get_data<real>().append(
+							std::abs(Impl::parse_field(fields[attrib.field_ids.front()])), (real)t_mod
+						);
+					}
+					else
+
+					/// \END hack
+					///////////
+
 					a.template get_data<real>().append(
 						Impl::parse_field(fields[attrib.field_ids.front()]), (real)t_mod
 					);
@@ -567,7 +615,7 @@ traj_dataset<flt_type> csv_handler<flt_type>::read (
 		traj_format_handler<flt_type>::attributes(ret).emplace(attr.name, std::move(attr.ds_attrib));
 	}
 	// prepare invented radii
-	auto R = traj_format_handler<flt_type>::template add_attribute<real>(ret, "radius");
+	auto R = traj_format_handler<flt_type>::template add_attribute<real>(ret, "_radius");
 
 	// set visual mapping
 	visual_attribute_mapping<real> vamap(impl.vmap_hints);
@@ -730,17 +778,17 @@ csv_imldevice_reg(
 
 // Register handler for streamline .csv files exported from paraview
 static const csv_descriptor csv_paraview_streamline_desc("Paraview Streamline", ",", {
-	{ "timestamp", {"IntegrationTime", false, 4}, CSV::TIMESTAMP },
-	{ "position",  {{"Points:0", false, 13}, {"Points:1", false, 14}, {"Points:2", false, 15}}, CSV::POS },
-	{ "normal", {{"Normals:0", false, 10}, {"Normals:1", false, 11}, {"Normals:2", false, 12}} },
-	{ "Normals:0", {"Normals:0", false, 10} }, // make individual
-	{ "Normals:1", {"Normals:1", false, 11} }, // components accessible
-	{ "Normals:2", {"Normals:2", false, 12} }, // also
-	{ "velocity",  {{"U:0", false, 0}, {"U:1", false, 1}, {"U:2", false, 2}} },
-	{ "vorticity", {{"Vorticity:0", false, 5}, {"Vorticity:1", false, 6}, {"Vorticity:2", false, 7}} },
-	{ "Vorticity:0", {"Vorticity:0", false, 5} }, // make individual
-	{ "Vorticity:1", {"Vorticity:1", false, 6} }, // components accessible
-	{ "Vorticity:2", {"Vorticity:2", false, 7} }  // also
+	{ "Time", {"IntegrationTime", false, 4}, CSV::TIMESTAMP },
+	{ "Position",  {{"Points:0", false, 13}, {"Points:1", false, 14}, {"Points:2", false, 15}}, CSV::POS },
+	{ "Normal", {{"Normals:0", false, 10}, {"Normals:1", false, 11}, {"Normals:2", false, 12}} },
+	{ "Normal.x", {"Normals:0", false, 10} }, // make individual
+	{ "Normal.y", {"Normals:1", false, 11} }, // components accessible
+	{ "Normal.z", {"Normals:2", false, 12} }, // also
+	{ "Velocity",  {{"U:0", false, 0}, {"U:1", false, 1}, {"U:2", false, 2}} },
+	{ "Vorticity", {{"Vorticity:0", false, 5}, {"Vorticity:1", false, 6}, {"Vorticity:2", false, 7}} },
+	{ "Vorticity.x", {"Vorticity:0", false, 5} }, // make individual
+	{ "Vorticity.y", {"Vorticity:1", false, 6} }, // components accessible
+	{ "Vorticity.z", {"Vorticity:2", false, 7} }  // also
 });
 //static const csv_descriptor csv_paraview_streamline_desc("Paraview Streamline", ",", {
 //	{ "timestamp", {"\"IntegrationTime\"", false, 4}, CSV::TIMESTAMP },
@@ -755,7 +803,7 @@ cgv::base::object_registration_2<
 	visual_attribute_mapping<float>({
 		{VisualAttrib::POSITION, {
 			// scale up dataset to make intersectors more numerically stable
-			"position", attrib_transform<float>::vec3_to_vec3(
+			"Position", attrib_transform<float>::vec3_to_vec3(
 				[](csv_handler<float>::Vec3& out, const csv_handler<float>::Vec3& in) {
 					out = 100.f*in;
 				}
@@ -763,7 +811,7 @@ cgv::base::object_registration_2<
 		}},
 		{VisualAttrib::RADIUS, {
 			// scale up radius accordingly but not as much to reduce overlapping tubes
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = 75.f*in;
 				}

commit 68bed309c1fbeed51bf081fe76d075eeced198ba

Fix crash when loading some .csv files introduced in 906d5b2bda11c2135af1d95e6cb50c1538cd46dd

csv_handler.cxx
@@ -725,7 +725,7 @@ cgv::base::object_registration_2<
 		 }},
 		{VisualAttrib::RADIUS, {
 			// we scale up the dataset to get more sensible numbers (mitigates floating point rounding errors etc.)
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = 128 * in;
 				}
@@ -759,7 +759,7 @@ csv_imldevice_reg(
 		 }},
 		{VisualAttrib::RADIUS, {
 			// we scale up the dataset to get more sensible numbers (mitigates floating point rounding errors etc.)
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = 128 * in;
 				}
@@ -845,7 +845,7 @@ cgv::base::object_registration_2<
 	visual_attribute_mapping<float>({
 		{VisualAttrib::RADIUS, {
 			// increase radius a bit to get thicker tubes with more visible area
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = 4.0f*in;
 				}
@@ -885,7 +885,7 @@ cgv::base::object_registration_2<
 		 }},
 		{VisualAttrib::RADIUS, {
 			// increase radius a bit to get thicker tubes with more visible area
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = in;
 				}
@@ -909,7 +909,7 @@ cgv::base::object_registration_2<
 	visual_attribute_mapping<float>({
 		{VisualAttrib::RADIUS, {
 			// increase radius a bit to get thicker tubes with more visible area
-			"radius", attrib_transform<float>::real_to_real(
+			"_radius", attrib_transform<float>::real_to_real(
 				[](float &out, const float &in) {
 					out = in;
 				}