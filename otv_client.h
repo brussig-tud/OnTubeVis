brussig-tud/OnTubeVis

commit 2d2c31eae91a71e6f50f69019c51819d8b9edeb3

More WIP refactoring for extrapolated trajectories

otv_client.h
@@ -166,9 +166,14 @@ struct otv_client {
 		return { id, *render.try_get_trajectory(id) };
 	}
 
+	/// Extract the arclength position of the first glyph pointed to by the given @c ro_range @a glyph_data.
+	[[nodiscard]] constexpr float get_glyph_pos (const ro_range<std::vector<float>::iterator> &glyph_data) {
+		return *glyph_data.begin;
+	}
+
 	/// perform necessary logic to enqueue glyphs. Returns the index of the first glyph within the input range that
-	/// needed to be added to an extrapolation.
-	unsigned enqueue_glyphs (
+	/// needed to be added to an extrapolation, if any.
+	std::optional<unsigned> enqueue_glyphs (
 		trajectory_ref target, unsigned layer, const ro_range<std::vector<float>::iterator> &glyph_data
 	);
 

render/state.h
@@ -2,6 +2,7 @@
 
 // C++ STL
 #include <bitset>
+#include <optional>
 
 // CGV framework GPU algorithms
 #include <cgv_gpgpu/visibility_sort.h>
@@ -147,15 +148,36 @@ public:
 		glyph_count_type  glyphs_per_trajectory
 	);
 
-	/// Calculate the extent of a glyph on the given layer along the trajectory, taking into account
-	/// the configured scale.
-	/// `glyph_data` must point to an array of attributes as they are stored in the render buffer,
-	/// including attributes that do not affect the size of the glyph, but excluding arc length and
-	/// debug info.
-	[[nodiscard]] float glyph_length(layer_index_type layer, const float *glyph_data) {
+	/// Calculate the extent of a glyph relative to its anchor point on the given layer along the trajectory, taking
+	/// into account the configured scale. Plot control points have flexible extents which cannot be determined in
+	/// isolation (they extend all the way to their neighboring control points), so in case of non-glyphs, a @a none
+	/// @c std::optional will be returned.
+	///
+	/// ToDo: `glyph_data` must point to an array of attributes as they are stored in the render buffer, including
+	/// attributes that do not affect the size of the glyph, but excluding arc length and debug info.
+	[[nodiscard]] std::optional<cgv::vec2> glyph_extents (layer_index_type layer, const float *glyph_data) {
 		// Streaming visualization only supports one dataset.
-		return visualizations[0].config.layer_configs[layer].glyph_length(glyph_data)
-			/ style.length_scale;
+		const float length_scale_x2 = (style.length_scale+style.length_scale); // to get radius instead of diameter
+		const float glyph_length = visualizations[0].config.layer_configs[layer].glyph_length(glyph_data);
+		if (glyph_length < 0)
+			return std::optional<unsigned>();
+		const float radius = std::max(
+			visualizations[0].config.layer_configs[layer].glyph_length(glyph_data) / length_scale_x2, 0.f
+		);
+		return std::make_optional<cgv::vec2>(-radius, radius);
+	}
+
+	/// Calculate the diameter of a glyph on the given layer along the trajectory, taking into account the configured
+	/// scale.
+	///
+	/// ToDo: `glyph_data` must point to an array of attributes as they are stored in the render buffer, including
+	/// attributes that do not affect the size of the glyph, but excluding arc length and debug info.
+	[[nodiscard]] std::optional<float> glyph_diameter (layer_index_type layer, const float *glyph_data) {
+		// Streaming visualization only supports one dataset.
+		const auto extents = glyph_extents(layer, glyph_data);
+		return map_optional(extents, [](const cgv::vec2 &extents) {
+			return extents.y() - extents.x();
+		});
 	}
 
 private:

otv_client.cxx
@@ -508,12 +508,40 @@ void otv_client::update ()
 	}
 }
 
-unsigned otv_client::enqueue_glyphs (
+std::optional<unsigned> otv_client::enqueue_glyphs (
 	trajectory_ref target, unsigned layer, const ro_range<std::vector<float>::iterator> &glyph_data
 ){
+	// Enqueue glyphs to "regular" trajectories
 	target.traj.enqueue_glyphs(layer, glyph_data);
+
+	// Find out how many of the glyphs we must also route to the current extrapolation
+	const unsigned num_glyphs = target.traj.attrib_to_glyph_count(layer, glyph_data.length()).value;
+	const unsigned stride = target.traj.glyph_to_attrib_count(layer, glyph_count_type{1});
+	const float smax = target.traj.arclength();
+	unsigned num_glyphs_nonextrapol = 0;
+	// - loop through new glyphs newest-to-oldest, to avoid needing special handling for plot control points
+	ro_range one_glyph = {glyph_data.end-stride, glyph_data.end};
+	float glyph_pos = get_glyph_pos(one_glyph);
+	auto glyph_extents = render.glyph_extents(layer, &*one_glyph.begin + 2)
+	                          	.value_or(cgv::vec2(.0f, .0f)); // no special handling for plot control points!
+	float glyph_max = glyph_pos + glyph_extents.y();
+	while (glyph_max >= smax && num_glyphs_nonextrapol < num_glyphs)
+	{
+		num_glyphs_nonextrapol++;
+		if (one_glyph.begin > glyph_data.begin) {
+			one_glyph -= stride;
+			glyph_pos = get_glyph_pos(one_glyph);
+			glyph_extents = render.glyph_extents(layer, &*one_glyph.begin + 2)
+			                	.value_or(cgv::vec2(.0f, .0f));  // again, no special handling required!
+			glyph_max = glyph_pos + glyph_extents.y();
+		}
+	}
+
+	// Done!
 	otv_instance->session_taa_keep_sampling = true;
-	return target.traj.attrib_to_glyph_count(layer, glyph_data.length()).value;
+	return num_glyphs_nonextrapol ?
+		std::make_optional(num_glyphs - num_glyphs_nonextrapol) : // Some(index)
+		std::optional<unsigned>(); // None
 }
 
 void otv_client::service_push_spline_node (unsigned traj_id, node_attribs &&node, const cgv::mat4 *t_to_s)

render/trajectory.cxx
@@ -108,6 +108,11 @@ void trajectory::append_node (const node_attribs &node, const cgv::mat4 *t_to_s)
 	});
 }
 
+float trajectory::arclength (void) const {
+	return _last_segment_idx != nil ? *_render.t_to_s[_last_segment_idx].end() : 0;
+}
+
+
 void trajectory::update_glyphs ()
 {
 	// Nothing to do if no layers changed.
@@ -171,7 +176,7 @@ void trajectory::update_glyphs ()
 				}
 
 				glyph_center = *seg_attribs.begin;
-				glyph_radius = _render.glyph_length(layer_idx, &*seg_attribs.begin + 2) * 0.5f;
+				glyph_radius = .5f * _render.glyph_diameter(layer_idx, &*seg_attribs.begin + 2).value_or(-2);
 
 				// Found a glyph potentially on the segment.
 				// Negative glyph length indicates potentially infinite extent.
@@ -210,7 +215,7 @@ void trajectory::update_glyphs ()
 				}
 
 				glyph_center = *seg_attribs.end;
-				glyph_radius = _render.glyph_length(layer_idx, &*seg_attribs.end + 2) * 0.5f;
+				glyph_radius = .5f * _render.glyph_diameter(layer_idx, &*seg_attribs.begin + 2).value_or(-2);
 			}
 
 			// Calculate the initial glyph range of this trajectory's next segment, consisting of

render/trajectory.h
@@ -73,7 +73,7 @@ public:
 	);
 
 	/// Release all resources associated with a glyph layer.
-	void destroy_glyph_layer(layer_index_type idx)
+	void destroy_glyph_layer (layer_index_type idx)
 	{
 		_layers[idx] = {};
 	}
@@ -83,6 +83,11 @@ public:
 	/// parametrization `t_to_s`.
 	/// If the trajectory is empty, no segment is created and `t_to_s` is ignored.
 	void append_node (const node_attribs &node, const cgv::mat4 *t_to_s);
+
+	/// report the current logical arclength of the whole trajectory. @a Logical here means that this value represents
+	/// the arclength since the very first node that was ever inserted, even if this node has long since been popped
+	/// off the tail end of the buffer.
+	float arclength (void) const;
 
 	/// Copy glyph attributes to a host-side buffer, from whence they will be added to the render
 	/// buffer on the next frame.

util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 
 
 /// A right-open range [begin, end).
@@ -23,7 +24,27 @@ struct ro_range {
 	{
 		return elem >= begin && elem < end;
 	}
+
+	ro_range& operator += (const std::size_t offset) noexcept {
+		begin += offset;
+		end += offset;
+		return *this;
+	}
+
+	ro_range& operator -= (const std::size_t offset) noexcept {
+		begin -= offset;
+		end -= offset;
+		return *this;
+	}
 };
 
 template <class T>
 ro_range(T, T) -> ro_range<T>;
+
+
+template <typename O, typename F>
+auto map_optional (O &&o, F &&f) -> std::optional<decltype(f(*std::forward<O>(o)))> {
+	if (!o.has_value())
+		return std::nullopt;
+	return {f(*std::forward<O>(o))};
+}

commit bbc89c18e535499015b8eaad4e9352110f4a06e1

Fix MSVC build errors introduced in 2d2c31eae91a71e6f50f69019c51819d8b9edeb3

otv_client.h
@@ -167,7 +167,7 @@ struct otv_client {
 	}
 
 	/// Extract the arclength position of the first glyph pointed to by the given @c ro_range @a glyph_data.
-	[[nodiscard]] constexpr float get_glyph_pos (const ro_range<std::vector<float>::iterator> &glyph_data) {
+	[[nodiscard]] float get_glyph_pos (const ro_range<std::vector<float>::iterator> &glyph_data) {
 		return *glyph_data.begin;
 	}
 

render/state.h
@@ -160,7 +160,7 @@ public:
 		const float length_scale_x2 = (style.length_scale+style.length_scale); // to get radius instead of diameter
 		const float glyph_length = visualizations[0].config.layer_configs[layer].glyph_length(glyph_data);
 		if (glyph_length < 0)
-			return std::optional<unsigned>();
+			return std::optional<cgv::vec2>();
 		const float radius = std::max(
 			visualizations[0].config.layer_configs[layer].glyph_length(glyph_data) / length_scale_x2, 0.f
 		);