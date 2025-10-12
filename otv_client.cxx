994cd5af52c32fc7a802c24e70c9422477ea18de
Paul Hempel <paul.hempel@mailbox.tu-dresden.de>
2025-10-12 15:32:43 +0200

    Fix rendering of extrapolated segments

--- otv_client.cxx
+++ otv_client.cxx
@@ -465,7 +465,7 @@ void otv_client::precompute_extrapolations (void)
 			const auto col = data->colors[node_idx];
 			const node_attribs cur_node {
 				{data->positions[node_idx], data->radii[node_idx]},
-				255.f*cgv::vec4{col.R(), col.G(), col.B(), 1},
+				cgv::vec4{col.R(), col.G(), col.B(), 1},
 				data->tangents[node_idx],
 				{data->timestamps[node_idx], 0, 0, 0}
 			};
@@ -574,7 +574,7 @@ void otv_client::update ()
 				target_traj.id, node_idx,
 				node_attribs{
 					{data->positions[node_idx], data->radii[node_idx]},
-					255.f*cgv::vec4{col.R(), col.G(), col.B(), 1},
+					cgv::vec4{col.R(), col.G(), col.B(), 1},
 					data->tangents[node_idx],
 					{data->timestamps[node_idx], 0, 0, 0}
 				}
