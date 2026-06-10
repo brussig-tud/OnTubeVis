2de44e39683f5750d8b60cea2e8f6dede8a35987
Paul Hempel <paul.hempel@tu-dresden.de>
2026-06-10 10:29:53 +0200

    [api/vis_setup] Reduce default amount of logging

--- api/api/vis_setup.cxx
+++ api/api/vis_setup.cxx
@@ -15,6 +15,9 @@
 #include <state/core.h>
 
 
+/// Controls logging verbosity. Higher values mean more messages.
+constexpr auto log_level = 1u;
+
 
 //////
 //
@@ -23,13 +26,15 @@
 
 OTV_API OTV_VisSetupHandle otv__create_VisSetup (const char *const name) {
 	auto new_vs = new VisSetup(name);
-	std::clog << "otv__create_VisSetup: created setup "<<hex(new_vs)<<std::dec<<" named '"<<new_vs->name<<"'."
-	          << std::endl;
+	if constexpr (log_level > 0)
+		std::clog << "otv__create_VisSetup: created setup "<<hex(new_vs)<<std::dec<<" named '"
+		          <<new_vs->name<<"'."<<std::endl;
 	return (OTV_VisSetupHandle)new_vs;
 }
 
 OTV_API void otv__free_VisSetup (OTV_VisSetupHandle vis_setup) {
-	std::clog << "otv__free_VisSetup: freeing setup "<<hex(vis_setup)<<std::dec<<"." << std::endl;
+	if constexpr (log_level > 0)
+		std::clog << "otv__free_VisSetup: freeing setup "<<hex(vis_setup)<<std::dec<<"." << std::endl;
 	delete (VisSetup*)vis_setup;
 }
 
@@ -42,8 +47,9 @@ OTV_API uint32_t otv__add_trajectory (OTV_VisSetupHandle vis_setup, const float 
 	const auto &traj = vs.trajs.emplace_back(
 		trajectory_setup{vs.counter++, radius}
 	);
-	std::clog << "otv__add_trajectory: added trajectory #"<<traj.id << std::endl
-	          << " - setup for '"+vs.name+"' now has "<<vs.trajs.size()<<" trajectorie(s)." << std::endl;
+	if constexpr (log_level > 1)
+		std::clog << "otv__add_trajectory: added trajectory #"<<traj.id << std::endl
+		          << " - setup for '"+vs.name+"' now has "<<vs.trajs.size()<<" trajectorie(s)." << std::endl;
 
 	return traj.id;
 }
@@ -63,8 +69,10 @@ OTV_API bool otv__add_layer (OTV_VisSetupHandle vis_setup, const OTV_LayerConfig
 	// Add the new layer
 	auto &new_layer = vs.layers.emplace_back(*config);
 	vs.layer_sources.emplace_back();
-	std::clog << "otv__add_layer: added layer of type '"<<otv__string_from_GlyphType(new_layer.type)<<"'" << std::endl
-	          << " - setup for '"+vs.name+"' now has "<<vs.layers.size()<<" layer(s)." << std::endl;
+	if constexpr (log_level > 0)
+		std::clog << "otv__add_layer: added layer of type '"<<otv__string_from_GlyphType(new_layer.type)
+		          <<"'" << std::endl
+		          << " - setup for '"+vs.name+"' now has "<<vs.layers.size()<<" layer(s)." << std::endl;
 	return true;
 }
 
