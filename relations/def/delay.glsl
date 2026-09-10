81ebab66c6011ce74f8a5753aedc61b3c18d01f2
Paul Hempel <paul.hempel@tu-dresden.de>
2026-09-10 17:08:46 +0200

    [relations/def] Improve the delay relation

--- relations/def/delay.glsl
+++ relations/def/delay.glsl
@@ -8,7 +8,7 @@ struct Reduction
 
 RELATION_REDUCE_T init_relation (TrajPoint base)
 {
-	return Reduction(-2, 0);
+	return Reduction(-1./0, 0);
 }
 void eval_relation (
 	TrajPoint base_point,
@@ -17,8 +17,9 @@ void eval_relation (
 	inout RELATION_REDUCE_T reduction
 ) {
 	const vec3 offset = sample_point.position - base_point.position;
-	const float alignment = dot(normalize(base_point.derivative), normalize(sample_point.derivative))
-		/ dot(offset, offset);
+	const float alignment =
+		dot(normalize(base_point.derivative), normalize(sample_point.derivative))
+		* (sqr(relation_radius[0]) - dot(offset, offset));
 
 	if (alignment > reduction.alignment) {
 		reduction.alignment = alignment;
@@ -27,9 +28,7 @@ void eval_relation (
 }
 vec3 color_relation (TrajPoint base, RELATION_REDUCE_T reduction)
 {
-	if (reduction.alignment < -1) return relation_background_color;
-
 	float delay = sign(reduction.delay) * (relation_radius[1] - abs(reduction.delay));
-	if (relation_normalize) delay /= 2 * relation_radius[1];
+	if (relation_normalize) delay /= relation_radius[1];
 	return relation_to_color(delay);
 }
